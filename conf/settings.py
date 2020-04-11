4448c711e1231dd3bfe510b89ecf57ec4136d541
unifiedsaad <saad.m@whyunified.com>
2020-04-11 20:59:13 +0500

    pacakge add

--- conf/settings.py
+++ conf/settings.py
@@ -132,7 +132,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
 # INSTALLED_APPS += ('debug_toolbar',)
-INTERNAL_IPS = ("127.0.0.1",)
+INTERNAL_IPS = ("127.0.0.1", "saadtesting.herokuapp.com")
 
 DEBUG_TOOLBAR_CONFIG = {"INTERCEPT_REDIRECTS": False, "TAG": "body"}
 
