27a1965fcad2e72b3aaaa6a945e1d7cf7f2d4042
unifiedsaad <saad.m@whyunified.com>
2020-04-11 21:11:00 +0500

    pacakge add

--- conf/settings.py
+++ conf/settings.py
@@ -55,19 +55,10 @@ MEDIA_URL = ""
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = "/static/"
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = "/static/"
 
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+
+
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -176,3 +167,11 @@ LOGGING = {
 # INVITATION_BACKEND = 'organizations.backends.MyInvitationBackend'
 # REGISTRATION_BACKEND = 'organizations.backends.MyRegistrationBackend'
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
